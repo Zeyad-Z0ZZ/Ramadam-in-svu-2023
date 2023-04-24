76f5a65ae78007790bcaaa03a28797d18ffd0159
Zeyad Mohammed AboSehly <92084044+Zeyad-Z0ZZ@users.noreply.github.com>
2023-04-24 02:01:25 +0200

    Create I. Nuts.cpp

---
+++ Marathon 2023/I. Nuts.cpp
@@ -0,0 +1,19 @@
+// problem link -> https://codeforces.com/group/QI9pPpkbDH/contest/434691/problem/I
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+
+int n;
+	cin >> n;
+	if(n==1)cout<< "Win";
+	else if(n==2)cout<< "Draw";
+	else cout<<"Lose";
+
+    return 0;
+}
